10b5e9e2d9fc32b9327977ef59d71278bc9a1b55
zhpan0016 <15820917056@163.com>
2023-02-04 22:08:30 +0800

    fix constructor.cpp

--- haizeix/cpp/capationHu/2.封装/5.constructor.cpp
+++ haizeix/cpp/capationHu/2.封装/5.constructor.cpp
@@ -2,28 +2,75 @@
 
 struct A
 {
-    A()
+    A() : next(nullptr)
     {
-        std::cout << "default constructor" << std::endl;
+        std::cout << this << " default constructor" << std::endl;
     }
-    A(int n)
+
+    A(A *obj)
     {
-        std::cout << "an int constructor" << std::endl; 
+        this->next = obj;
+        this->next->arr = new int[1000];
+    }
+
+    A(int n) : next(nullptr)
+    {
+        std::cout << this << " an int constructor" << std::endl;
         x = n;
         y = n;
     }
+
+    A(const A &obj) : next(nullptr)
+    {
+        std::cout << this << " copy constructor" << std::endl;
+        this->x = obj.x;
+        this->y = obj.y;
+    }
+
     void output()
     {
-        std::cout << this->x << " " << this->y << std::endl; 
+        std::cout << this->x << " " << this->y << std::endl;
     }
+
     int x, y;
+    int *arr;
+    A *next;
+    ~A()
+    {
+        if (this->next)
+        {
+            delete this->next->arr;
+        }
+        std::cout << this << " destructor" << std::endl;
+    }
 };
+
+void func(int &x)
+{
+    x += 5;
+}
+
+A aa;
+A bb(&aa);
+A cc(&bb);
 
 int main()
 {
+    int n = 7;
+    func(n);
+    std::cout << n << std::endl;
     std::cout << "pre position" << std::endl;
-    A a();
-    A b(6);
+    A a;
+    A b = 8;
+    A c(a);
+    std::cout << "a = " << &a << std::endl;
+    std::cout << "b = " << &b << std::endl;
+    std::cout << "c = " << &c << std::endl;
+    std::cout << "aa = " << &aa << std::endl;
+    std::cout << "bb = " << &bb << std::endl;
+    std::cout << "cc = " << &cc << std::endl;
+    b.output();
+    b = 6;
     b.output();
     std::cout << "next position" << std::endl;
     return 0;
